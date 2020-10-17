304a89c04f88fe96cfeeaf4e86efa9f8db5e9441
Six Jonathan <j.six@student.isartdigital.com>
2020-10-17 10:01:43 +0200

    Add basic menu and add function to quit and load new scene

---
+++ Assets/Scripts/Manager/MySceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MySceneManager : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {}
+
+    public void LoadNewSceneWithIndex(int index)
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneAt(index).ToString(), LoadSceneMode.Single);
+    }
+
+    public void LoadNewSceneWithName(string name)
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+
+    public void QuiteGame()
+    {
+        Application.Quit();
+    }
+}
