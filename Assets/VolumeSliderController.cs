965d30e45d62ebcf8c66eb9b51ce98af2ce8403f
Six Jonathan <j.six@student.isartdigital.com>
2020-10-17 21:30:23 +0200

    Add menu in game

---
+++ Assets/VolumeSliderController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+public class VolumeSliderController : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdatePictureVolume()
+    {
+        image.sprite = sprites[(int)(AudioListener.volume * sprites.Length)];
+    }
+
+    public void ChangeGeneralVolume(float newVolume)
+    {
+        AudioListener.volume = newVolume;
+        UpdatePictureVolume();
+    }
+}
