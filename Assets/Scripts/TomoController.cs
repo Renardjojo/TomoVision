d8fdac25f34afa017379df21245f9e7bee63301d
Six Jonathan <j.six@student.isartdigital.com>
2020-10-17 15:29:00 +0200

    Interrgation of graphisme and done level one. Mecanic not done for now

---
+++ Assets/Scripts/TomoController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TomoController : MonoBehaviour
+{
+    int direction = 0;
+
+    [SerializeField]
+    public GameObject TomoViewObject;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TurnLeft()
+    {
+        TomoViewObject.transform.GetChild(direction).gameObject.SetActive(false);
+        direction++;
+        
+        if (direction >= TomoViewObject.transform.childCount)
+        {
+            direction = 0;
+        }
+
+        TomoViewObject.transform.GetChild(direction).gameObject.SetActive(true);
+    }
+
+    public void TurnRight()
+    {
+        TomoViewObject.transform.GetChild(direction).gameObject.SetActive(false);
+        direction--;
+
+        if (direction < 0)
+        {
+            direction = TomoViewObject.transform.childCount - 1;
+        }
+
+        TomoViewObject.transform.GetChild(direction).gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/TransparentSelector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TransparentSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
