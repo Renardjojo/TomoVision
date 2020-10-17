12d4627102c93dd0cdb6e1e3294259747a71cf88
Six Jonathan <j.six@student.isartdigital.com>
2020-10-17 03:39:10 +0200

    Add button and selection ability. Create base architecture of the project

---
+++ Assets/Scripts/Manager/InputController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+public class InputController : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask layerValidForSelection = 8;
+
+    Selectable currentObjectSelected = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {      
+            RaycastHit2D rayHit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition), Mathf.Infinity, layerValidForSelection);
+
+            if (rayHit)
+            {
+                if (currentObjectSelected != null)
+                {
+                    currentObjectSelected.Unselect();
+                }
+
+                currentObjectSelected = rayHit.transform.gameObject.GetComponent<Selectable>();
+                currentObjectSelected.Select();
+            }
+            else if (currentObjectSelected != null)
+            {
+                currentObjectSelected.Unselect();
+                currentObjectSelected = null;
+            }
+        } 
+    }
+}
---
+++ Assets/Scripts/Selectable/Selectable.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour
+{
+    protected SpriteRenderer spriteRenderer;
+    protected bool bIsSelected = false;
+
+    // Start is called before the first frame update
+    protected void Awake()
+    {
+       spriteRenderer = GetComponent<SpriteRenderer>(); 
+    }
+
+    protected void Start()
+    {}
+
+    void Update()
+    {}
+
+    public void Select()
+    {
+        bIsSelected = true;
+        SetOutlineEnable(true);
+        Debug.Log("ok");
+    }
+
+    public void Unselect()
+    {
+        bIsSelected = false;
+        SetOutlineEnable(false);
+    }
+
+    public void SwitchSelectionState()
+    {
+        bIsSelected = !bIsSelected;
+        SetOutlineEnable(bIsSelected);
+    }
+
+    public void SetOutlineEnable(bool bNewFlag)
+    {
+        if (bNewFlag)
+        {
+            spriteRenderer.material.SetFloat("_OutlineEnabled", 1f);
+        }
+        else
+        {
+            spriteRenderer.material.SetFloat("_OutlineEnabled", 0f);
+        }
+    }
+
+    public void SetOutlineThickness(float NewThickness)
+    {
+        spriteRenderer.material.SetFloat("_Thickness", NewThickness);
+    }
+
+    public void SetOutlineColor(Color NewColor)
+    {
+        MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+        spriteRenderer.GetPropertyBlock(mpb);
+
+        mpb.SetColor("_SolidOutline", NewColor);
+
+        spriteRenderer.SetPropertyBlock(mpb);
+    }
+
+}
